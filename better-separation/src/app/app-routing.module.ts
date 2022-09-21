9087d3ffa16b86f974e3ea55ed3c9757716e4731
Selin Sakarya <ext1443@foss.dk>
2022-09-21 17:19:54 +0300

    Add retailer guard

--- better-separation/src/app/app-routing.module.ts
+++ better-separation/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RetailerGuard } from './core/guards/retailer.guard';
 
 const routes: Routes = [
   {
@@ -12,6 +13,7 @@ const routes: Routes = [
   },
   {
     path: 'retailer',
+    canLoad: [RetailerGuard],
     loadChildren: () => import('./retailer/retailer.module').then(m => m.RetailerModule)
   }];
 
